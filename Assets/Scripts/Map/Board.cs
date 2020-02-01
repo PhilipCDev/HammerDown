ece757580c2398bf6a73f9b637bd9f6faa7ea6e5
Andreas Hanuja <hanuja2@gmx.de>
2020-02-01 16:08:57 +0100

    board update

--- Assets/Scripts/Map/Board.cs
+++ Assets/Scripts/Map/Board.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace HammerDown.Map
 {
@@ -8,9 +9,31 @@ namespace HammerDown.Map
         {
             return true;
         }
+
+        public bool IsCovered(RectanglePos position)
+        {
+            return true;
+        }
+
+        public bool IsPlankFixed(RectanglePos position, List<Vector2> nailPositions)
+        {
+            return false;
+        }
+
         public Vector2 GetSize()
         {
             return Vector2.one;
+        }
+    }
+
+    public struct RectanglePos{
+        public Vector2 leftBottom;
+        public Vector2 rightTop;
+
+        public RectanglePos(Vector2 _leftBottom, Vector2 _rightTop)
+        {
+            leftBottom = _leftBottom;
+            rightTop = _rightTop;
         }
     }
 }
