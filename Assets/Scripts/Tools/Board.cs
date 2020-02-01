369769a3bc6746dab530aad46d4846ca3ac7443b
Hannah <hannah.kriegler@aws-institut.de>
2020-02-01 12:24:49 +0100

    added draft of animals, boad, nail classed

---
+++ Assets/Scripts/Tools/Board.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HammerDown.Interfaces;
+using HammerDown.Tools;
+using UnityEngine;
+
+namespace HammerDown.Tools
+{
+    public class Board : MonoBehaviour, IGrabable
+    {
+        public float timeToFallDown;
+        private bool _isOnWall = false;
+        private int nails = 0;
+
+        public void OnGrab(Hand hand)
+        {
+            gameObject.transform.parent = hand.transform;
+        }
+
+        public void OnRelease(Hand hand)
+        {
+            gameObject.transform.parent = null;
+            // TODO Check if wall is free
+            _isOnWall = true;
+            StartCoroutine(nameof(FallDown));
+        }
+
+        private IEnumerator FallDown()
+        {
+            yield return new WaitForSeconds(timeToFallDown);
+            if (nails <= 3)
+            {
+                gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+
+
+        }
+        
+    }
+    
+}
