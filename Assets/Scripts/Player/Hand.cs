d0dd4932abcbd741940f6f2ba9282ad846abd056
Philip_Dev <philipchell@hotmail.com>
2020-02-01 14:20:18 +0100

    added hammer, hand interaction with interfaces

--- Assets/Scripts/Player/Hand.cs
+++ Assets/Scripts/Player/Hand.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 using HammerDown.Interfaces;
 
 namespace HammerDown.Player
@@ -8,6 +9,11 @@ namespace HammerDown.Player
     public class Hand : MonoBehaviour
     {
         Movement movement;
+
+        List<GameObject> grabbedObjectsInRange = new List<GameObject>();
+        List<GameObject> touchedObjects = new List<GameObject>();
+
+        GameObject activeGrabbedObject;
 
         private void Start()
         {
@@ -23,14 +29,83 @@ namespace HammerDown.Player
                 GrabEnd();
         }
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.GetComponent<ITouchable>() != null)
+            {
+                ITouchable[] touches = activeGrabbedObject.GetComponentsInChildren<ITouchable>();
+                foreach (ITouchable t in touches)
+                {
+                    t.OnTouchEnter(this);
+                }
+            }
+
+            if (other.gameObject.GetComponent<IGrabable>() != null)
+            {
+                if (!grabbedObjectsInRange.Contains(other.gameObject))
+                    grabbedObjectsInRange.Add(other.gameObject);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if(other.gameObject.GetComponent<ITouchable>() != null)
+            {
+                ITouchable[] touches = activeGrabbedObject.GetComponentsInChildren<ITouchable>();
+                foreach (ITouchable t in touches)
+                {
+                    t.OnTouchExit(this);
+                }
+            }
+
+            if (other.gameObject.GetComponent<IGrabable>() != null)
+            {
+                if (grabbedObjectsInRange.Contains(other.gameObject))
+                    grabbedObjectsInRange.Remove(other.gameObject);
+            }
+        }
+
         void GrabBegin()
         {
-            movement.MoveDown();
+            if (grabbedObjectsInRange.Count == 0)
+            {
+                Debug.Log("No grabbable object in range!");
+                return;
+            }
+
+            float minDistance = float.MaxValue;
+            GameObject nearObject = null;
+            foreach (GameObject obj in grabbedObjectsInRange)
+            {
+                float dis = Vector3.Distance(obj.transform.position, transform.position);
+                if(dis < minDistance)
+                {
+                    minDistance = dis;
+                    nearObject = obj;
+                }
+            }
+
+            activeGrabbedObject = nearObject;
+            if(activeGrabbedObject != null)
+            {
+                IGrabable[] grabs = activeGrabbedObject.GetComponentsInChildren<IGrabable>();
+                foreach (IGrabable g in grabs)
+                {
+                    g.OnGrab(this);
+                }
+            }
         }
 
         void GrabEnd()
         {
-            movement.MoveUp();
+            if (activeGrabbedObject != null)
+            {
+                IGrabable[] grabs = activeGrabbedObject.GetComponentsInChildren<IGrabable>();
+                foreach (IGrabable g in grabs)
+                {
+                    g.OnRelease(this);
+                }
+            }
         }
     }
 }
