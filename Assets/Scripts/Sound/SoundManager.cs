e5e24f348a39bb3066bccdcfec475325a641f4f0
Andreas Hanuja <hanuja2@gmx.de>
2020-02-02 15:10:03 +0100

    sound

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HammerDown.Sound
+{
+    public enum SoundEffects {HAMMER, HURT, ANIMALDIE, NAILBROKE, SUCCESS};
+    public class SoundManager : MonoBehaviour
+    {
+        private static SoundManager current;
+        public AudioClip[] soundEffects;
+
+        public AudioClip menuMusic;
+        public AudioClip musicIngame;
+
+        public AudioListener cameraEar;
+
+        void Start()
+        {
+            current = this;
+        }
+
+        public static void PlaySound(SoundEffects effect)
+        {
+            AudioSource.PlayClipAtPoint(current.soundEffects[(int)effect], Camera.main.transform.position);
+        }
+
+        public static void SetMusic(bool menu)
+        {
+
+        }
+    }
+
+}
