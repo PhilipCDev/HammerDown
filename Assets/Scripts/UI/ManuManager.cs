610231bac9037b59ac40c035e5b9a3fa6db79606
NutPea <p.nuss96@outlook.de>
2020-02-02 14:18:18 +0100

    Menu Stuff

--- Assets/Scripts/UI/ManuManager.cs
+++ Assets/Scripts/UI/ManuManager.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class ManuManager : MonoBehaviour
+namespace HammerDown.UI
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public class ManuManager : MonoBehaviour
     {
-        
+        public Animator anim;
+        public GameObject HTP;
+        public GameObject Menu;
+        // Start is called before the first frame update
+        void Start()
+        {
+            HTP.SetActive(false);
+            Menu.SetActive(true);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        public void howToPlay()
+        {
+            HTP.SetActive(false);
+            Menu.SetActive(false);
+            anim.SetBool("goToHTP", true);
+        }
+        public void backToMenu()
+        {
+            HTP.SetActive(false);
+            Menu.SetActive(true);
+            anim.SetBool("goToHTP", false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
