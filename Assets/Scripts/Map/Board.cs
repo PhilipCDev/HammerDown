f391fab7ee617a8133d20cefcb1232699a6222a8
Andreas Hanuja <hanuja2@gmx.de>
2020-02-02 01:12:17 +0100

    temp

--- Assets/Scripts/Map/Board.cs
+++ Assets/Scripts/Map/Board.cs
@@ -1,35 +1,79 @@
 ﻿using HammerDown.Map.Components;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace HammerDown.Map
 {
     public class Board : MonoBehaviour
     {
+        private RectanglePos boardPos;
+        private float planeOffset;
 
-        // !!!! Added this here, maybe wrong but needed it somewhere
+        //Hannah: Move to Game
         public BoardStatus boardStatus;
         
         //List of all holes, just use and not change entries! Currently holes are not implemented
         public List<Hole> holes;
 
+        #region private Functions
+        private void Start()
+        {
+            CalculateBoardPos();
+        }
+        private void CalculateBoardPos()
+        {
+            //Bounds bounds = new Bounds(transform.position, Vector3.zero);
+            List<Bounds> bounds = gameObject.GetComponentsInChildren<Transform>()
+                .Where(t => t.gameObject.layer == 12)
+                .Select<Transform, Bounds>(t => t.gameObject.GetComponent<Collider>().bounds)
+                .ToList();
+
+            Bounds b = bounds[0];
+            for (int i=1; i<bounds.Count; i++)
+            {
+                b.Encapsulate(bounds[i].min);
+            }
+
+            if(b.extents.x < b.extents.z)
+            {
+                boardPos = new RectanglePos(new Vector2(b.center.z-b.extents.z,b.center.y-b.extents.y), new Vector2(b.center.z + b.extents.z, b.center.y + b.extents.y));
+            }
+            else
+            {
+                boardPos = new RectanglePos(new Vector2(b.center.x - b.extents.x, b.center.y - b.extents.y), new Vector2(b.center.x + b.extents.x, b.center.y + b.extents.y));
+            }
+            planeOffset = Vector3.Dot(b.center, transform.forward);
+            Debug.Log(planeOffset);
+        }
+        #endregion
+
+        #region public Functions
         //Bool function. Give a point and returns if it is on a board. Check for Nail (Nail can only be nailed in if OnBoard is true)
         public bool IsOnBoard(Vector2 position)
         {
-            return true;
+            //TODO: think of problem with 45 deg plane
+            //Hannah: Speak about forward direction, meaning of x
+            Vector3 startPos = transform.up * position.y + transform.right * position.x + transform.forward * (planeOffset + 1);
+            RaycastHit[] hits = Physics.RaycastAll(startPos, -transform.forward, 1, 1 << 12 | 1 << 14);
+            if (hits.Length > 0 && !hits.Any(h => h.collider.gameObject.layer == 14))
+            {
+                return true;
+            }
+            return false;
         }
 
         //Call Whenever a nail is nailed completly in. Returns true only if the Plank is fully fixed. Needs position of the board and position of all full nailed in and not broken nails.
         public bool IsPlankFixed(RectanglePos position, List<Vector2> nailPositions)
         {
-            // TODO hab ich gecallt, soll ich was mit dem return machen?
+            // Hannah: Yes, use for call BoardStatus.AddFixedPlanks
             return false;
         }
 
-        //returns size of the board
-        public Vector2 GetSize()
+        //Hannah: Changed, returns RectanglePos of the board
+        public RectanglePos GetBoardPos()
         {
-            return Vector2.one;
+            return boardPos;
         }
 
         //used for evaluating points, you do not need this
@@ -37,6 +81,7 @@ namespace HammerDown.Map
         {
             return 0;
         }
+        #endregion
     }
 
     public struct RectanglePos{
--- Assets/Scripts/Map/BoardStatus.cs
+++ Assets/Scripts/Map/BoardStatus.cs
@@ -5,51 +5,95 @@ using UnityEngine;
 
 namespace HammerDown.Map
 {
+    //Do TODOs, Discuss Hannah:
     public class BoardStatus : MonoBehaviour
     {
-        private HashSet<Plank> UnusedPlanks; //Planks that can potential be moved. Potential moved != !Fixed. Fixed only if 3-4 Nails completly in. Potential move as long no nail in
-        private HashSet<Nail> UnusedNails; //Nails that can potential be moved. That means you can still move them or hit them by a hammer
-        private HashSet<Plank> FixedPlanks; //Planks that are fixed. Board.IsPlankFixed return true for nails that are in the plank
+        private HashSet<Plank> UnusedPlanks = new HashSet<Plank>(); //Planks that can potential be moved. Potential moved != !Fixed. Fixed only if 3-4 Nails completly in. Potential move as long no nail in
+        private HashSet<Nail> UnusedNails = new HashSet<Nail>(); //Nails that can potential be moved. That means you can still move them or hit them by a hammer
+        private HashSet<Plank> FixedPlanks = new HashSet<Plank>(); //Planks that are fixed. Board.IsPlankFixed return true for nails that are in the plank
 
+        private bool isChecking = false;
+
+        #region privateFunctions
         private void CheckStatus()
         {
-
+            if(UnusedNails.Count == 0 && UnusedPlanks.Count == 0)
+            {
+                //TODO: END
+                ResetStatus();
+            }
+            else
+            {
+                if (false)//TODO: Check if all holes are covered
+                {
+                    //TODO: END
+                    ResetStatus();
+                }
+            }
         }
+        private void ResetStatus()
+        {
+            UnusedPlanks = new HashSet<Plank>();
+            UnusedNails = new HashSet<Nail>();
+            FixedPlanks = new HashSet<Plank>();
+            isChecking = false;
+        }
+        #endregion
 
+        #region public Functions
         //call to add plank to unused planks. Call for each plank in current game. Not in Scene, in Game!
         public void AddPlank(Plank plank)
         {
-
+            UnusedPlanks.Add(plank);
         }
 
         //call to add nail to unused nail. Call for each plank in current game. Not in Scene, in Game!
         public void AddNail(Nail nail)
         {
-
+            UnusedNails.Add(nail);
         }
 
         //Call after all Nails and Boards are added
         public void StartChecking()
         {
-            // TODO where should I call this? can you call it after removing it from a list? - Hannah
+            isChecking = true;
+            //Hannah: where should I call this? can you call it after removing it from a list? - Hannah
         }
 
         //call to remove plank from unused planks. Call when plank is attached by one nail (just single stroke in the wall), or when gameobject is whyever destroyed
         public void RemovePlank(Plank plank)
         {
-
+            if (UnusedPlanks.Contains(plank))
+            {
+                UnusedPlanks.Remove(plank);
+                if (isChecking)
+                {
+                    CheckStatus();
+                }
+            }
         }
 
         //call to remove nail from unused nails. Call when nail is attached completely in the wall or has been hit wrong (is broken), or when gameobject is whyever destroyed
         public void RemoveNail(Nail nail)
         {
-            // it could happen, that i remove it multiple times from the list 
+            //Hannah: it could happen, that i remove it multiple times from the list 
+            if (UnusedNails.Contains(nail))
+            {
+                UnusedNails.Remove(nail);
+                if (isChecking)
+                {
+                    CheckStatus();
+                }
+            }
         }
 
         //call to add plank to fixed planks. Call when Board.IsPlankFixed return true
-        public void AddFixedPlanks(Plank plank) // Nail nail) // changed type from nail to plank?
+        public void AddFixedPlanks(Plank plank) 
         {
-
+            //Hannah:  Nail nail changed type from nail to plank? Correct
+            FixedPlanks.Add(plank);
+            CheckStatus();
         }
+        #endregion
     }
 }
